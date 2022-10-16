4283b8d93a765980780690e0aa0bc3dd2fb6457e
Natalia Venkatesh <nataliavenkatesh@MacBook-Pro.local>
2022-10-16 11:26:53 -0400

    locak icon added console log cleaned up

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ var descTime = document.getElementsByClassName("description");
  Array.from(descTime).forEach(description => {
   let pHour  =  parseInt(description.id)
   
-    console.log(currentHour)
+    // console.log(currentHour)
 
     if (pHour){
 
@@ -29,15 +29,15 @@ var descTime = document.getElementsByClassName("description");
 
 
 var currentTime = moment();
-console.log(currentTime)
+// console.log(currentTime)
 
 function saveTask (event){
-    console.log("click")
-    console.log(event)
+    // console.log("click")
+    // console.log(event)
     const text=event.target.previousElementSibling.value
     const hour=event.target.previousElementSibling.id
     const tasks=JSON.parse(localStorage.getItem("planner")) || {}
-    console.log (tasks)
+    // console.log (tasks)
     tasks[hour]=text
     localStorage.setItem("planner", JSON.stringify(tasks))
     $("#message").text("Your task has been saved!")
@@ -52,13 +52,13 @@ function saveTask (event){
 function loadTasks(){
  const tasks = JSON.parse(localStorage.getItem("planner")) || {}
  for(let i = 9; i<=17; i++){
-    console.log($("#"+i),tasks[i])
+    // console.log($("#"+i),tasks[i])
     $("#"+i).val(tasks[i] || "")
 
  }
 }
 
-$("#currentDay").text(currentTime.format(" D MMM YYYY"))
+$("#currentDay").text(currentTime.format("dddd, MMMM Do"))
 $(".saveBtn").on("click", saveTask)
 
 loadTasks()
